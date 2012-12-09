fbafffbe287f2d6b1f1ca78336cae6c19f92f508
Denis Neuling <denisneuling@gmail.com>
2012-12-09 23:27:16 +0100

    Introduce spring application context

---
+++ db2eshop-governance/src/main/java/com/db2eshop/governance/spring/ApplicationContextLoader.java
@@ -0,0 +1,35 @@
+package com.db2eshop.governance.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextLoader {
+
+	public static final String classpathConfigurationLocation = "classpath*:applicationContext.xml";
+	private ApplicationContext applicationContext;
+
+	private static ApplicationContextLoader INSTANCE;
+	
+	public static ApplicationContextLoader getInstance(){
+		if(INSTANCE == null){
+			INSTANCE = new ApplicationContextLoader();
+		}
+		return INSTANCE;
+	}
+	
+	private ApplicationContextLoader(){
+		applicationContext = new ClassPathXmlApplicationContext(classpathConfigurationLocation);
+	}
+
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+	
+	public static void main(String[] args){
+		ApplicationContextLoader.getInstance();
+	}
+}
