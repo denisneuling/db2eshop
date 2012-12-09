c255f17edcb5ae863d7866f3e8fe0eda44ea2424
Denis Neuling <denisneuling@gmail.com>
2012-12-10 00:03:36 +0100

    Add UIComponent annotation

---
+++ db2eshop-annotations/src/main/java/com/db2eshop/annotations/bindings/UIComponent.java
@@ -0,0 +1,14 @@
+package com.db2eshop.annotations.bindings;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface UIComponent {
+
+	Class<java.awt.Component> value();
+	
+}
