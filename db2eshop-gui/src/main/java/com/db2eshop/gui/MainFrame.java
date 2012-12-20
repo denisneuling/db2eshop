4d04e7be10ab021a35dce7f8ae4c8a066ef4dfe2
Denis Neuling <denisneuling@gmail.com>
2012-12-20 20:17:33 +0100

    Add miglayout to dependencies

--- db2eshop-gui/src/main/java/com/db2eshop/gui/MainFrame.java
+++ db2eshop-gui/src/main/java/com/db2eshop/gui/MainFrame.java
@@ -2,12 +2,22 @@ package com.db2eshop.gui;
 
 import javax.swing.JFrame;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.db2eshop.persistence.LieferantenDao;
 
 @Component
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 5447391288059975630L;
 
+	@Autowired
+	private LieferantenDao lieferantenDao;
 	
+	public void doSMT(){
+		String addresse = "asdasd";
+		String name = "sdsdf"  ; 
+		lieferantenDao.findBy("name", "addresse", name , addresse);
+	}
 
 }
