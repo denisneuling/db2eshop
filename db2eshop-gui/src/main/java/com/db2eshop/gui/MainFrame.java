e0ba7c210398d3ac6e4124132407d91f846e4547
Denis Neuling <denisneuling@gmail.com>
2012-12-31 02:10:28 +0100

    fix project compile errors

--- db2eshop-gui/src/main/java/com/db2eshop/gui/MainFrame.java
+++ db2eshop-gui/src/main/java/com/db2eshop/gui/MainFrame.java
@@ -1,9 +1,12 @@
 package com.db2eshop.gui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 import net.miginfocom.swing.MigLayout;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,27 +18,29 @@ import org.springframework.stereotype.Component;
  */
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 5447391288059975630L;
-	
-	setLayout(new MigLayout("", "[grow]", "[]5[]"));
-	  add(new JLabel("KNr"), "");
-	  add(new JTextField(5), "wrap");
-	  add(new JLabel("Vorname"), "");
-	  add(new JTextField(10), "");
-	  add(new JLabel("Nachname"), "");
-	  add(new JTextField(10), "wrap");
-	  add(new JLabel("Strasse"), "");
-	  add(new JTextField(17), "");
-	  add(new JLabel("HNr"), "");
-	  add(new JTextField(3), "wrap");
-	  add(new JLabel("PLZ"), "");
-	  add(new JTextField(5), "");
-	  add(new JLabel("Ort"), "");
-	  add(new JTextField(15), "wrap");
-	  
-	  add(new JButton("Exit"), "center");
-	  
-	  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  pack();
-	  setLocationRelativeTo(null);
-	  setVisible(true);
+
+	public MainFrame() {
+		setLayout(new MigLayout("", "[grow]", "[]5[]"));
+		add(new JLabel("KNr"), "");
+		add(new JTextField(5), "wrap");
+		add(new JLabel("Vorname"), "");
+		add(new JTextField(10), "");
+		add(new JLabel("Nachname"), "");
+		add(new JTextField(10), "wrap");
+		add(new JLabel("Strasse"), "");
+		add(new JTextField(17), "");
+		add(new JLabel("HNr"), "");
+		add(new JTextField(3), "wrap");
+		add(new JLabel("PLZ"), "");
+		add(new JTextField(5), "");
+		add(new JLabel("Ort"), "");
+		add(new JTextField(15), "wrap");
+
+		add(new JButton("Exit"), "center");
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
 }
