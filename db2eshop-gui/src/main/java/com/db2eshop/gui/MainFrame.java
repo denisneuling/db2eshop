8875cc006824215428af478f37176ea0024e09c7
Dennis Wieding <e-dennis@gmx.net>
2012-12-31 02:03:36 +0100

    Introducing MicLayout

--- db2eshop-gui/src/main/java/com/db2eshop/gui/MainFrame.java
+++ db2eshop-gui/src/main/java/com/db2eshop/gui/MainFrame.java
@@ -6,8 +6,6 @@ import net.miginfocom.swing.MigLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.db2eshop.persistence.LieferantenDao;
-
 @Component
 /**
  * <p>MainFrame class.</p>
@@ -17,18 +15,27 @@ import com.db2eshop.persistence.LieferantenDao;
  */
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 5447391288059975630L;
-
-	@Autowired
-	private LieferantenDao lieferantenDao;
 	
-	/**
-	 * <p>doSMT.</p>
-	 */
-	public void doSMT(){
-		String addresse = "asdasd";
-		String name = "sdsdf"  ; 
-		lieferantenDao.findBy("name", "addresse", name , addresse);
-	}
-
 	setLayout(new MigLayout("", "[grow]", "[]5[]"));
+	  add(new JLabel("KNr"), "");
+	  add(new JTextField(5), "wrap");
+	  add(new JLabel("Vorname"), "");
+	  add(new JTextField(10), "");
+	  add(new JLabel("Nachname"), "");
+	  add(new JTextField(10), "wrap");
+	  add(new JLabel("Strasse"), "");
+	  add(new JTextField(17), "");
+	  add(new JLabel("HNr"), "");
+	  add(new JTextField(3), "wrap");
+	  add(new JLabel("PLZ"), "");
+	  add(new JTextField(5), "");
+	  add(new JLabel("Ort"), "");
+	  add(new JTextField(15), "wrap");
+	  
+	  add(new JButton("Exit"), "center");
+	  
+	  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  pack();
+	  setLocationRelativeTo(null);
+	  setVisible(true);
 }
