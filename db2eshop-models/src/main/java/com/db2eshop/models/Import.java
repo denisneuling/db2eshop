5a4a5b1a0b6ca8dceb7b15313d3c566825b971eb
Denis Neuling <denisneuling@gmail.com>
2013-01-05 22:34:43 +0100

    fix compilation errors and add jpa annotations

--- db2eshop-models/src/main/java/com/db2eshop/models/Import.java
+++ db2eshop-models/src/main/java/com/db2eshop/models/Import.java
@@ -1,9 +1,14 @@
 package com.db2eshop.models;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.db2eshop.models.support.AbstractModel;
 
@@ -14,28 +19,29 @@ import com.db2eshop.models.support.AbstractModel;
  * @author Denis Neuling (denisneuling@gmail.com)
  * @version $Id: $Id
  */
-public class Import extends AbstractModel<Import> {
-	private static final long serialVersionUID = ;
+public class Import extends AbstractModel<Import> implements Serializable{
+	private static final long serialVersionUID = -8663449394006905708L;
 
-
+	@Id
+	@GeneratedValue
 	private Long id;
 
-
+	@Column
 	private int count;
 	
-	
-	private int date;
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date;
 
-	
 	public Import() {
 	}
 
-
+	@Override
 	public Long getId() {
 		return id;
 	}
 
-
+	@Override
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -52,7 +58,7 @@ public class Import extends AbstractModel<Import> {
 		return count;
 	}
 
-	public void setDate(int date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 }
