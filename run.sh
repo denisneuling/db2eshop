9a9dafa16075e1f7c85a14088d3b7c4eafb8f7a1
Denis Neuling <denisneuling@gmail.com>
2013-01-07 17:42:09 +0100

    Project reorganization

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp db2eshop-gui/target/db2eshop-gui-0.0.1-SNAPSHOT-jar-with-dependencies.jar -jar db2eshop-gui/target/db2eshop-gui-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -cp target/db2eshop-0.0.1-SNAPSHOT-jar-with-dependencies.jar -jar target/db2eshop-0.0.1-SNAPSHOT-jar-with-dependencies.jar
